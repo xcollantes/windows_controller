c992f048da88a7d4866739f4123d79868c7ededb
Xavier Collantes <collantes.xavier@gmail.com>
2021-11-13 00:56:14 -0800

    Add eth run

--- startup_manager/startup_manager.py
+++ startup_manager/startup_manager.py
@@ -2,22 +2,56 @@
 
 import os
 import sys
+import time
+import subprocess
 import logging
-from typing import List
-
+from pywinauto import Desktop
 from pywinauto.application import Application
 
-logging.basicConfig(level=logging.INFO)
 
-# https://pywinauto.readthedocs.io/en/latest/getting_started.html#getting-started-guide
-BACKEND_TYPE = "uia"
+logging.basicConfig(level=logging.DEBUG)
+# Needed for elevated access.
+os.environ.update({"__COMPAT_LAYER": "RUnAsInvoker"})
+
+MINING_BATCH_PATH = (
+    "C:\\Users\\colla\\Documents\\cryptomining\\trex\\xavier-eth.bat")
 
 
-def onStart() -> None:
-    logging.info("Starting Startup Manager")
-    notepadApp = Application(backend=BACKEND_TYPE).start("notepad.exe")
+def startTrexMiner(pathExe: os.path) -> None:
+    logging.info("Starting Trex Miner")
 
+
+def isRunning(programPattern: str) -> bool:
+    logging.info("Looking for program with pattern %s", programPattern)
+    desktopApps = Desktop().windows()
+
+    for program in desktopApps:
+        if programPattern in program.window_text():
+            logging.info("%s running", program.window_text())
+            return True
+    logging.info("None found with pattern: %s", programPattern)
+    return False
+
+
+def startMiningOperation() -> None:
+    logging.info("Starting mining ops")
+    if not isRunning("MSI A"):
+        logging.info("MSI not running, starting up")
+        msiClock = Application().start(
+            "C:\\Program Files (x86)\\MSI Afterburner\\MSIAfterburner.exe")
+
+    # time.sleep(5)  # Make sure MSI is running
+
+    if not isRunning("xavier-eth") and isRunning("MSI A"):
+        callFile = os.path.basename(MINING_BATCH_PATH)
+        callDir = os.path.dirname(MINING_BATCH_PATH)
+
+        os.system(f"""
+            cd {callDir} && {callFile}
+        """)
+        logging.info(os.getcwd())
+        
 
 
 if __name__ == "__main__":
-    onStart()
+    startMiningOperation()
