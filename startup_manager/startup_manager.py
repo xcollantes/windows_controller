70ba061b71c6a4e3f48001696eb34c081b04d449
Xavier Collantes <collantes.xavier@gmail.com>
2021-11-17 19:37:41 -0800

    Add debug

--- startup_manager/startup_manager.py
+++ startup_manager/startup_manager.py
@@ -18,22 +18,38 @@ MINING_BATCH_PATH = (
 
 
 def startTrexMiner(pathExe: os.path) -> None:
+    """Start Ethereum script.
+
+    Args:
+        pathExe: Full escaped path of script to run.
+    """
     logging.info("Starting Trex Miner")
 
 
 def isRunning(programPattern: str) -> bool:
-    logging.info("Looking for program with pattern %s", programPattern)
+    """Check if a given program is active.
+
+    Args:
+        programPattern: String of window title; may not be complete.
+                        Ex: to find Spotify, "Spo" or "Spotif" will work.
+
+    Returns:
+        True if program is active, False otherwise.
+    """
+    logging.debug("Looking for program with pattern %s", programPattern)
     desktopApps = Desktop().windows()
 
     for program in desktopApps:
         if programPattern in program.window_text():
-            logging.info("%s running", program.window_text())
+            logging.debug("%s running", program.window_text())
             return True
-    logging.info("None found with pattern: %s", programPattern)
+    logging.warn("None found with pattern: %s", programPattern)
     return False
 
 
 def startMiningOperation() -> None:
+    """Start up overclocking software to regulate GPU, start Ethereum script.
+    """
     logging.info("Starting mining ops")
     if not isRunning("MSI A"):
         logging.info("MSI not running, starting up")
@@ -45,12 +61,12 @@ def startMiningOperation() -> None:
     if not isRunning("xavier-eth") and isRunning("MSI A"):
         callFile = os.path.basename(MINING_BATCH_PATH)
         callDir = os.path.dirname(MINING_BATCH_PATH)
+        logging.debug(f"Running command: `cd {callDir} && {callFile}`")
 
         os.system(f"""
             cd {callDir} && {callFile}
         """)
         logging.info(os.getcwd())
-        
 
 
 if __name__ == "__main__":
